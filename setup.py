4e7ef3e3e217e3462bae5ae712af5a41a1bc9790
gk22aan <gauravk612@gmail.com>
2024-06-20 19:27:41 +0100

    Ml project initial commit using cookie cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='small project for emotion detection of twitter data',
+    author='gk22aan',
+    license='',
+)
